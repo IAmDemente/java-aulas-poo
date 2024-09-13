3413bed10392db532acafb56013ead48a4ae3deb
Gustavo F. <167870640+IAmDemente@users.noreply.github.com>
2024-09-13 11:30:45 -0300

    Associando valores a objetos

--- src/main/java/br/sesi/gfd/poo/Main.java
+++ src/main/java/br/sesi/gfd/poo/Main.java
@@ -1,10 +1,28 @@
 package br.sesi.gfd.poo;
 
+import br.sesi.gfd.poo.cabeca.Cabeca;
+import br.sesi.gfd.poo.cabeca.Olho;
+
 public class Main {
     public static void main(String args []){
 
+        Olho olho1 = new Olho();
+        olho1.cego = true;
+        olho1.diametroOlho = 2;
+        olho1.corIris = "azul";
+
+        Olho olho2 = new Olho();
+        olho2.cego = false;
+        olho2.diametroOlho = 2;
+        olho2.corIris = "castanho";
+
+        Cabeca cabeca1 = new Cabeca();
+        cabeca1.olhoDireito = olho2;
+        cabeca1.olhoEsquerdo = olho1;
+
         Pessoa pessoa1 = new Pessoa();
 
-        pessoa1.cabeca.zoio.corIris = "marrom";
+        pessoa1.cabeca = cabeca1;
     }
+
 }
--- src/main/java/br/sesi/gfd/poo/cabeca/Cabeca.java
+++ src/main/java/br/sesi/gfd/poo/cabeca/Cabeca.java
@@ -2,13 +2,15 @@ package br.sesi.gfd.poo.cabeca;
 
 public class Cabeca {
 
-    public Olho zoio;
+    public Olho olhoEsquerdo;
+    public Olho olhoDireito;
     public Orelha orelhao;
     public Nariz nariz;
     public Boca boca;
 
     public Cabeca(){
-        this.zoio = new Olho();
+        this.olhoDireito = new Olho();
+        this.olhoEsquerdo = new Olho();
         this.orelhao = new Orelha();
         this.nariz = new Nariz();
         this.boca = new Boca();
